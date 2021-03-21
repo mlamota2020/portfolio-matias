9d3bb983c0227b4641fc851ef62236170bfbbb89
Matias La Mota <matiaslamota18@gmail.com>
2021-03-21 16:41:04 +0000

    Some things

--- main.js
+++ main.js
@@ -58,98 +58,32 @@ formulary.addEventListener('input', filter);
 
 // Ejecución correcta del formulario
 
-const formulario = document.getElementById('formulario');
-const inputs = document.querySelectorAll('#formulario input');
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
 
-const expresiones = {
-  usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-  nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-  password: /^[a-zA-ZÀ-ÿ\s]{4,1100}$/, // 4 a 12 digitos.
-  correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-  telefono: /^\d{7,14}$/ // 7 a 14 numeros.
-}
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
 
-const campos = {
-  usuario: false,
-  nombre: false,
-  password: false,
-  correo: false,
-  telefono: false
-}
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
 
-const validarFormulario = (e) => {
-  switch (e.target.name) {
-    case "usuario":
-      validarCampo(expresiones.usuario, e.target, 'usuario');
-      break;
-    case "nombre":
-      validarCampo(expresiones.nombre, e.target, 'nombre');
-      break;
-    case "password":
-      validarCampo(expresiones.password, e.target, 'password');
-      break;
-    case "password2":
-      validarCampo(expresiones.password, e.target, 'password2');;
-      break;
-    case "correo":
-      validarCampo(expresiones.correo, e.target, 'correo');
-      break;
-    case "telefono":
-      validarCampo(expresiones.telefono, e.target, 'telefono');
-      break;
-  }
-}
-
-const validarCampo = (expresion, input, campo) => {
-  if (expresion.test(input.value)) {
-    document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
-    document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
-    document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
-    document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
-    document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
-    campos[campo] = true;
-  } else {
-    document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
-    document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
-    document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
-    document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
-    document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
-    campos[campo] = false;
-  }
-}
-
-
-
-inputs.forEach((input) => {
-  input.addEventListener('keyup', validarFormulario);
-  input.addEventListener('blur', validarFormulario);
-});
-
-formulario.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  const terminos = document.getElementById('terminos');
-  if (campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked) {
-    formulario.reset();
-
-    document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
-    setTimeout(() => {
-      document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
-    }, 5000);
-
-    document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
-      icono.classList.remove('formulario__grupo-correcto');
-    });
-  } else {
-    document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
-  }
-});
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
 
 // Envío de recibo a contacto
 
-const formv = document.querySelector('#correo')
-const form = document.querySelector('.formulario__btn');
-const namem = document.querySelector('#nombre');
+const formv = document.querySelector('#validationTooltip05')
+const form = document.querySelector('#btn');
+const namem = document.querySelector('#validationTooltip01');
 
 form.addEventListener('click', sendEmail);
 
